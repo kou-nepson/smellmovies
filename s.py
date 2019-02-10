104529084bd38a8b4fd3b7a5d63125d695c3da70
杉山航 <cjgjc803@gmail.com>
2019-02-10 10:26:41 +0900

    ファイル操作の修正

--- s.py
+++ s.py
@@ -1,4 +1,5 @@
 import urllib3
+import os
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -9,18 +10,29 @@ soup = BeautifulSoup(bsURL.text, 'html.parser')
 review_texts = soup.select(".p-mark__review")
 #print(review_texts)
 
+move_title = str(soup.select(".p-content-detail__title > span")[0].text)
+# dir_n = re.match(r'.*<span>(.*?)</span>.*', move_title).group(1)
 last_page = soup.select_one(".c-pagination__last")
 if last_page:
         last_page_num= int(last_page.get('href').split("=")[-1])
 else:
         last_page_num = 0
+# print(dir_n)
+print(move_title)
 for zero in range(last_page_num):
     hoge_url = url + '?page='+str(zero)+''
     hoge_bsURL = requests.get(hoge_url)
     hoge_soup = BeautifulSoup(hoge_bsURL.text, 'html.parser')
     review_texts += hoge_soup.select(".p-mark__review")
 print(str(review_texts))
-path = 'review.txt'
-
+path = move_title
+if os.path.exists(path) == False:
+    os.makedirs(path)
+if os.path.exists(path) == True:
+    print("すでに完了しています")
+path += '/review.txt'
+tmppath = 'list.txt'
 with open(path, mode='w') as f:
     f.write(str(review_texts))
+with open(tmppath, mode='w') as g:
+    g.write(move_title)
