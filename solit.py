98b14d6d2653938fed5e136a4f437cf0a6bc2800
杉山航 <cjgjc803@gmail.com>
2019-02-12 15:46:52 +0900

    膨大なデータでも分かち書きができるように修正

--- solit.py
+++ solit.py
@@ -1,3 +1,4 @@
+
 import MeCab
 import gensim
 import sys
@@ -11,9 +12,12 @@ with open("newreview.txt","w") as n:
         text = binarydata.decode('utf_8')
         text = re.sub(r'<br/>', '  ', text)
         text = re.sub(r'<br>', '  ', text)
-        text = re.sub(r'</br>', '  ', text)
+        text = re.sub(r'</br>', '\n', text)
         text = re.sub(r'<div class="p-mark__review">', '\n', text)
         text = re.sub(r'</div>', '', text)
+        text = re.sub(r'記録', '', text)
+#        text = re.sub(r'[', '', text)
+ #       text = re.sub(r']', '', text)
        # text = re.sub(r'<div id="kashi_area" itemprop="text">', '', text)
         n.write(text)
     print(text)
@@ -21,19 +25,22 @@ with open("newreview.txt","w") as n:
 m = MeCab.Tagger("-Owakati")
 
 with open("wakachi.txt","w") as f:
-    result = m.parse(text)
-    print(result)
-    f.write(result) # skip first \s
+    text1 = text.splitlines()
+    result = ""
+    for t in text1:
+        result += m.parse(t)
+#        print(result)
+    f.write(str(result)) # skip first \s
 
-print(result[0])
+#print(result[0])
 
 #sentences = word2vec.LineSentence("wakachi.txt")
-sentences = word2vec.Text8Corpus("wakachi.txt")
-model = word2vec.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
-vector = model.wv["僕"]
-word = model.most_similar( [ vector ], [], 5)
-print(vector)
-print(word)
+#sentences = word2vec.Text8Corpus("wakachi.txt")
+#model = word2vec.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+#vector = model.wv["僕"]
+#word = model.most_similar( [ vector ], [], 5)
+#print(vector)
+#print(word)
 
 #model = word2vec.Word2Vec(sentences,
  #                         sg=1,
