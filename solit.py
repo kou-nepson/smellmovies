82bfea226086b7ffc0b2cf57022c84aca0e72c86
杉山航 <cjgjc803@gmail.com>
2019-02-13 16:48:20 +0900

    ファイルの管理を圧倒的にしやすく、おそらくいろんな環境で動かせる

--- solit.py
+++ solit.py
@@ -1,4 +1,4 @@
-
+import os
 import MeCab
 import gensim
 import sys
@@ -6,31 +6,41 @@ import re
 from gensim.models import word2vec
 
 sys.path.append("/.pyenv/versions/anaconda3-4.3.1/envs/word2vec/lib/python3.6/site-packages")
-with open("newreview.txt","w") as n:
-    with open("review.txt","rb") as f:
-        binarydata = f.read()
-        text = binarydata.decode('utf_8')
-        text = re.sub(r'<br/>', '  ', text)
-        text = re.sub(r'<br>', '  ', text)
-        text = re.sub(r'</br>', '\n', text)
-        text = re.sub(r'<div class="p-mark__review">', '\n', text)
-        text = re.sub(r'</div>', '', text)
-        text = re.sub(r'記録', '', text)
+#dirname = os.path.dirname('/data/')
+path = os.getcwd()
+path += "/data"
+#print(str(dirname))
+files = os.listdir(path)
+print(files)
+files.pop(0) #.DS_Storeを削除
+for i in files:
+    with open("data/"+str(i)+"/newreview.txt","w") as n:
+        with open("review.txt","rb") as f:
+            binarydata = f.read()
+            text = binarydata.decode('utf_8')
+            text = re.sub(r'<br/>', '  ', text)
+            text = re.sub(r'<br>', '  ', text)
+            text = re.sub(r'</br>', '\n', text)
+            text = re.sub(r'<div class="p-mark__review">', '\n', text)
+            text = re.sub(r'</div>', '', text)
+            text = re.sub(r'記録', '', text) #フィルマークスで記録とかくだけのレビューが大量にいるため
 #        text = re.sub(r'[', '', text)
  #       text = re.sub(r']', '', text)
        # text = re.sub(r'<div id="kashi_area" itemprop="text">', '', text)
-        n.write(text)
-    print(text)
+            n.write(text)
+            print(i)
 
 m = MeCab.Tagger("-Owakati")
 
-with open("wakachi.txt","w") as f:
-    text1 = text.splitlines()
-    result = ""
-    for t in text1:
-        result += m.parse(t)
+for j in files:
+    with open("data/"+str(j)+"/wakachi.txt","w") as f:
+        text1 = text.splitlines()
+        result = ""
+        for t in text1:
+            result += m.parse(t)
+        print(j)
 #        print(result)
-    f.write(str(result)) # skip first \s
+        f.write(str(result)) # skip first \s
 
 #print(result[0])
 
